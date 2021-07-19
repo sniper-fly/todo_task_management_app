e84b00c09adf37788480af5e3a9d9b4c0aa4e83a
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-19 18:56:48 +0900

    タスク機能の使用に認証をかけるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /tasks or /tasks.json
   def index
@@ -22,6 +23,7 @@ class TasksController < ApplicationController
   # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params)
+    @task.user_id = current_user.id
 
     respond_to do |format|
       if @task.save
