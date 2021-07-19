a5b44206a1e5e932c8347a5b6541e5067c61fdd6
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-19 18:59:02 +0900

    userとtaskのアソシエーションを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  belongs_to :worker, class_name: 'User', foreign_key: 'worker_id', required: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :tasks, dependent: :destroy #destroyじゃないようにもできるようにしたい
+  has_many :tasks_to_do, foreign_key: 'worker_id', class_name: 'Task'
 end
