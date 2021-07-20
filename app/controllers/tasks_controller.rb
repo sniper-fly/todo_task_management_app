e19cd259e5535f46f0320426e9b89fdf0460bc2c
RyoNakai <ryo.nakai@super-studio.jp>
2021-07-20 11:23:54 +0900

    タスクにステータス情報を入力できるようにした

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -66,6 +66,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :content)
+      params.require(:task).permit(:title, :content, :status)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,21 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :worker, class_name: 'User', foreign_key: 'worker_id', required: false
+
+  enum statuses: {
+    waiting: 0,
+    doing: 1,
+    done: 2
+  }
+
+  STATUS_NAME = {
+    Task.statuses[:waiting] => '未対応',
+    Task.statuses[:doing] => '対応中',
+    Task.statuses[:done] => '完了'
+  }
+
+  def get_status_str
+    STATUS_NAME[self.status]
+  end
+
 end
